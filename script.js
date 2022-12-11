103e16f9c12da6c5ec20b558b42c7cbff9e53a8b
MasumHossain71 <masumhazi311@gmail.com>
2022-12-11 15:27:16 +0600

    section style added

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ addSec.innerHTML = `
       <li>Valorant</li>
    </ul>
 </section>
+<section>
+   <h1>My Favorite Dress.</h1>
+   <ul>
+      <li>T-Shirt</li>
+      <li>Pant</li>
+      <li>Full Sleeve Shirt.</li>
+   </ul>
+</section>
 `;
 createSection.appendChild(addSec);
+addSec.style.border = '2px solid red';
+addSec.style.backgroundColor = 'gray';
+addSec.style.color = 'white';
+addSec.style.textAlign = 'center';
+addSec.style.fontSize = '2em';
